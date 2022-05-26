fdf2858e4be4cf8e6fdb1f642490bbbab256b1df
Nicholas Bruce <saltair93+github@gmail.com>
2022-05-26 07:45:47 -0700

    generate env works

--- rfind-web/rfind_web/environment/utils.py
+++ rfind-web/rfind_web/environment/utils.py
@@ -1,53 +1,70 @@
-from pathlib import Path
 import warnings
+from pathlib import Path
 
-from .defaults import DEFAULT_SOCKETIO_VARS, DEFAULT_DATA_VARS, DEFAULT_S3_VARS, DEFAULT_SIM_VARS
+from .defaults import (
+    DEFAULT_DATA_VARS,
+    DEFAULT_S3_VARS,
+    DEFAULT_SIM_VARS,
+    DEFAULT_SOCKETIO_VARS,
+)
 
-def get_env_dir()->dict:
+
+def get_env_dir() -> Path:
     """
     Get the directory of the .env file
     """
-    return Path(__file__).parent.parent
+    return Path(__file__).parent.parent.parent
 
 
-def get_env_files()->list:
+def get_env_files() -> list:
     """
     Get the list of environment files
     """
     env_dir = get_env_dir()
-    env_files = env_dir.glob('**.env**')
-    return env_files
+    env_files = env_dir.glob("**.env**")
+    return list(env_files)
 
-def load_env()->dict:
+
+def load_env() -> dict:
     """
     Load environment variables from .env file
     """
     env_dir = get_env_dir()
 
     try:
-        env = file_to_dict(env_dir+'.env.local')
+        env = file_to_dict(env_dir / ".env.local")
     except FileNotFoundError:
-        warnings.warn('No .env.local file found in {}. Defaulting to rfind_web.environment.defaults'.format(env_dir))
+        warnings.warn(
+            "No .env.local file found in {}. Defaulting to"
+            " rfind_web.environment.defaults".format(env_dir)
+        )
         env = get_all_defaults()
-    
+
     return env
 
-def file_to_dict(p:Path)->dict:
+
+def file_to_dict(p: Path) -> dict:
     """
     Convert a env formatted file to a dictionary.
     TODO could be replaced with python-dotenv package
     """
-    env={}
+    env = {}
     with open(p) as f:
         for line in f:
-            if line.startswith('#'):
+            if line.startswith("#"):
                 continue
-            key, val = line.split('=')
+            key, val = line.split("=")
             env[key] = val.strip()
     return env
 
-def get_all_defaults()->dict:
+
+def get_all_defaults() -> dict:
     """
     Get the default values for all environment variables
     """
-    return {**DEFAULT_SOCKETIO_VARS, **DEFAULT_DATA_VARS, **DEFAULT_S3_VARS, **DEFAULT_SIM_VARS}
+    return {
+        **DEFAULT_SOCKETIO_VARS,
+        **DEFAULT_DATA_VARS,
+        **DEFAULT_S3_VARS,
+        **DEFAULT_SIM_VARS,
+    }
